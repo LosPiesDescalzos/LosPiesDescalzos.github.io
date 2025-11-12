34a067391579e541ca0fcf1ad033ef96798030d7
LosPiesDescalzos <katyaj2001@mail.ru>
2025-11-13 00:48:16 +0300

    fix: add modules

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,10 @@ export default defineConfig({
     outDir: 'docs',
     assetsDir: 'assets',
     emptyOutDir: true
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    }
   }
 })
