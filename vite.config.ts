46eb613315c0b684e008963579a6eaf87a91b480
LosPiesDescalzos <katyaj2001@mail.ru>
2025-11-13 00:01:07 +0300

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+// import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@recipes': path.resolve(__dirname, './src/resipes-data')
-    }
-  },
+  // resolve: {
+  //   alias: {
+  //     '@recipes': path.resolve(__dirname, './src/resipes-data')
+  //   }
+  // },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
